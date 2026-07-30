685a8f637447c6c31fa9c371b347069a65ab4cea
Mehul Jain <mehulj999@hotmail.com>
2026-07-31 04:52:04 +0530

    Add: evaluate-reverse-polish-notation - submission-7

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-7.py
@@ -0,0 +1,38 @@
+# class Solution:
+#     def evalRPN(self, tokens: List[str]) -> int:
+#         polishStack = []
+#         for token in tokens:
+#             if token in ["+", "-", "*", "/"]:
+#                 right = polishStack.pop()
+#                 left = polishStack.pop()
+#                 if token == '+':
+#                     result = left + right
+#                 elif token == '-':
+#                     result = left - right
+#                 elif token == '*':
+#                     result = left * right
+#                 elif token == '/':
+#                     result = int(left / right)
+#                 # result = int(eval(f"{left} {token} {right}"))
+#                 polishStack.append(result)
+#             else:
+#                 polishStack.append(int(token))
+#         return int(polishStack[-1])
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        for c in tokens:
+            if c == "+":
+                stack.append(stack.pop() + stack.pop())
+            elif c == "-":
+                a, b = stack.pop(), stack.pop()
+                stack.append(b - a)
+            elif c == "*":
+                stack.append(stack.pop() * stack.pop())
+            elif c == "/":
+                a, b = stack.pop(), stack.pop()
+                stack.append(int(float(b) / a))
+            else:
+                stack.append(int(c))
+        return stack[0]
